9c1c0ccc6cc9c2e6a655cdc9b55095131d20fec9
mingyuchoo <mingyuchoo@gmail.com>
2024-12-18 11:41:29 +0900

    updated

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Define a functino to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    // Call the function
+    let message = from "F#"
+    printfn "Hello world %s" message
+    0 // return an integer exit code
