10bccb04e464a1f9f2a7096a1864397965ba7cd4
mingyuchoo <mingyuchoo@address.com>
2025-08-28 04:24:36 +0900

    style: F# 코드 컨벤션에 맞게 함수 호출 전 공백 추가

--- MyFuncUI/Program.fs
+++ MyFuncUI/Program.fs
@@ -11,22 +11,22 @@ open Avalonia.Layout
 
 module Main =
 
-    let view() =
-        Component(fun ctx ->
+    let view () =
+        Component (fun ctx ->
             let state = ctx.useState 0
 
             DockPanel.create [
                 DockPanel.children [
                     Button.create [
                         Button.dock Dock.Bottom
-                        Button.onClick(fun _ -> state.Set(state.Current - 1))
+                        Button.onClick (fun _ -> state.Set (state.Current - 1))
                         Button.content "-"
                         Button.horizontalAlignment HorizontalAlignment.Stretch
                         Button.horizontalContentAlignment HorizontalAlignment.Center
                     ]
                     Button.create [
                         Button.dock Dock.Bottom
-                        Button.onClick(fun _ -> state.Set(state.Current + 1))
+                        Button.onClick (fun _ -> state.Set (state.Current + 1))
                         Button.content "+"
                         Button.horizontalAlignment HorizontalAlignment.Stretch
                         Button.horizontalContentAlignment HorizontalAlignment.Center
@@ -36,33 +36,33 @@ module Main =
                         TextBlock.fontSize 48.0
                         TextBlock.verticalAlignment VerticalAlignment.Center
                         TextBlock.horizontalAlignment HorizontalAlignment.Center
-                        TextBlock.text(string state.Current)
+                        TextBlock.text (string state.Current)
                     ]
                 ]
             ]
         )
 
-type MainWindow() =
-    inherit HostWindow()
+type MainWindow () =
+    inherit HostWindow ()
 
     do
         base.Title <- "Counter Example"
-        base.Content <- Main.view()
+        base.Content <- Main.view ()
 
-type App() =
-    inherit Application()
+type App () =
+    inherit Application ()
 
-    override this.Initialize() =
-        this.Styles.Add(FluentTheme())
+    override this.Initialize () =
+        this.Styles.Add (FluentTheme ())
         this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
 
-    override this.OnFrameworkInitializationCompleted() =
+    override this.OnFrameworkInitializationCompleted () =
         match this.ApplicationLifetime with
-        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow()
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow ()
         | _ -> ()
 
 module Program =
 
     [<EntryPoint>]
-    let main(args : string[]) =
-        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime(args)
+    let main (args : string[]) =
+        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime (args)
